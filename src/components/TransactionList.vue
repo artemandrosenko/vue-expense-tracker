6dbb815c3dd1dc3f942a639c0952827e0d85da4c
Artem Androsenko <tyoma.and@gmail.com>
2023-12-11 13:09:48 +0200

    Add the TransactionList component

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li class="minus">Cash
+        <span>-100</span>
+        <button class="delete-btn">x</button>
+    </li>
+    <li class="plus">Salary
+        <span>2400</span>
+        <button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
