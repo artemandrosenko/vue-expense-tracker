423b2b0464b3824ff5a08b81c9e8e5b5d2f0ede0
Artem Androsenko <tyoma.and@gmail.com>
2023-12-13 22:05:57 +0200

    Add a total balance calc

--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,4 +1,16 @@
 <template>
   <h4>Your Balance</h4>
-  <h2 id="balance">$0.00</h2>
+  <h2 id="balance">${{ total }}</h2>
 </template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true,
+  }
+});
+
+</script>
