e58551511b467dbe8d81bdf2bddfbca209aa3cd9
Artem Androsenko <tyoma.and@gmail.com>
2023-12-16 14:43:03 +0200

    Add new transaction to the State

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,66 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total"/>
+    <Balance :total="+total" />
     <IncomeExpenses :income="+income" :expenses="+expenses" />
     <TransactionList :transactions="transactions" />
-    <AddTransaction />
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
 
 <script setup>
-import Header from './components/Header.vue';
-import Balance from './components/Balance.vue';
-import IncomeExpenses from './components/IncomeExpenses.vue';
-import TransactionList from './components/TransactionList.vue';
-import AddTransaction from './components/AddTransaction.vue';
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
 
-import { ref, computed } from 'vue';
+import { ref, computed } from 'vue'
 
 const transactions = ref([
   { id: 1, text: 'Flower', amount: -19.99 },
   { id: 2, text: 'Salary', amount: 2400.99 },
   { id: 3, text: 'Book', amount: -10 },
   { id: 4, text: 'Phone', amount: -150 }
-]);
+])
 
 const total = computed(() => {
   return transactions.value.reduce((acc, transaction) => {
-    return transaction.amount + acc;
-  }, 0);
-});
+    return transaction.amount + acc
+  }, 0)
+})
 
 // Get income
 const income = computed(() => {
   return transactions.value
     .filter((transaction) => transaction.amount > 0)
     .reduce((acc, transaction) => {
-      return transaction.amount + acc;
+      return transaction.amount + acc
     }, 0)
-    .toFixed(2);
-});
+    .toFixed(2)
+})
 
 // Get expenses
 const expenses = computed(() => {
   return transactions.value
     .filter((transaction) => transaction.amount < 0)
     .reduce((acc, transaction) => {
-      return transaction.amount + acc;
+      return transaction.amount + acc
     }, 0)
-    .toFixed(2);
-});
+    .toFixed(2)
+})
+
+// Add transaction
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateId(),
+    text: transactionData.text,
+    amount: transactionData.amount
+  })
+}
+
+// Generate unique ID
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000)
+}
 </script>
